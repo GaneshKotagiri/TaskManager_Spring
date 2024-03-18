7202ea24330fbe426d261c3904e45a40ef757232
Ganesh Kotagiri <123595702+Ganeshkotagiri-12@users.noreply.github.com>
2024-03-18 18:09:59 -0400

    intial project via spring initializr
    Signed-off-by: Ganesh Kotagiri <123595702+Ganeshkotagiri-12@users.noreply.github.com>

---
+++ src/test/java/com/annotations/taskmanager_spring/TaskManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annotations.taskmanager_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
